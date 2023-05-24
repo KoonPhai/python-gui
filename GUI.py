c2e9250874b74c89c0383453cfc10b93f6bb8014
PhaiPhattarapong <paikung01@gmail.com>
2023-05-24 12:51:29 +0700

    Update GUI.py

---
+++ GUI.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+    window = tk.Tk()
+    window.title('JustPython')
+    window.minsize(width=400, height=400)
+
+    title_label = tk.Label(master=window, text='สูตรคูณแม่')
+    title_label.pack()
+    number_input = tk.Entry(master=window)
+    number_input.pack()
+    go_button = tk.Button(master=window, text='ผลลัพ')
+    go_button.pack()
+
+window.mainloop()
