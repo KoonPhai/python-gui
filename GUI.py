cc85c92f6eac7d19a683aed4716dcdb1818ff045
PhaiPhattarapong <paikung01@gmail.com>
2023-05-24 12:57:11 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 
-    window = tk.Tk()
-    window.title('JustPython')
-    window.minsize(width=400, height=400)
+window = tk.Tk()
+window.title('JustPython')
+window.minsize(width=400, height=400)
 
-    title_label = tk.Label(master=window, text='สูตรคูณแม่')
-    title_label.pack()
-    number_input = tk.Entry(master=window)
-    number_input.pack()
-    go_button = tk.Button(master=window, text='ผลลัพ')
-    go_button.pack()
+title_label = tk.Label(master=window, text='สูตรคูณแม่')
+title_label.pack()
+number_input = tk.Entry(master=window)
+number_input.pack()
+go_button = tk.Button(master=window, text='ผลลัพ')
+go_button.pack()
+output_label = tk.Label(master=window)
+output_label.pack()
 
 window.mainloop()
