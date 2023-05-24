07d13226243ebd3494ed5f9a17ac15c3d2acfa19
PhaiPhattarapong <paikung01@gmail.com>
2023-05-24 13:06:51 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 
+def show_output():
+    number = int(number_input.get())
+
+    output = ''
+    for i in range(1, 13):
+        output += str(number) + ' x ' + str(i)
+        output += ' = ' + str(number * i) + '\n'
+
 window = tk.Tk()
 window.title('JustPython')
 window.minsize(width=400, height=400)
@@ -8,7 +16,10 @@ title_label = tk.Label(master=window, text='สูตรคูณแม่')
 title_label.pack()
 number_input = tk.Entry(master=window)
 number_input.pack()
-go_button = tk.Button(master=window, text='ผลลัพ')
+go_button = tk.Button(
+    master=window, text='ผลลัพ' ,
+    command=show_output
+    )
 go_button.pack()
 output_label = tk.Label(master=window)
 output_label.pack()
